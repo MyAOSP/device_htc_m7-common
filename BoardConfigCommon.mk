7d6bd75584aaa8bcd06a507aaabfb1c4fa65a2e5
drgravy <drg113001@gmail.com>
2013-05-19 12:51:48 -0500

    TARGET_USES_QCOM_COMPRESSED_AUDIO

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,10 +42,10 @@ COMMON_GLOBAL_CFLAGS += -DNEW_ION_API=1
 COMMON_GLOBAL_CFLAGS += -DHTCLOG
 
 # Audio
-BOARD_AUDIO_CAF_LEGACY_INPUT_BUFFERSIZE := true
 BOARD_USES_FLUENCE_INCALL := true  # use DMIC in call only
 BOARD_USES_SEPERATED_AUDIO_INPUT := true  # use distinct voice recog/camcorder use cases
 BOARD_USES_SEPERATED_VOICE_SPEAKER := true  # use distinct voice speaker use case
+TARGET_USES_QCOM_COMPRESSED_AUDIO := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH_BCM := true
